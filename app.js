ee4cd5990087a0ede9ee292acd8b9565e04e2694
DanielMalede <danielmalede@gmail.com>
2022-11-15 20:30:15 +0200

    .

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require("http");
 
+///מתרגם לנו את המלל שיש לנו body ב
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
+
+app.use('/ddd',(req,res)=>{
+    res.send("h")
+})
 app.use('/',(req,res)=>{
     res.send("hello")
 })
+
 
 app.listen(3000);
